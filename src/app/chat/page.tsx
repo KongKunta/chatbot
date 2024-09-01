62631e53640fd8b34aac4236680ab406faa776b7
KongKunta <kongkunta@yahoo.com>
2024-09-01 12:22:03 +0200

    added redirect from chat to dynamic route with chatId as dynamicroute id

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,14 +3,18 @@ import Chat from "@/app/lib/modals/chat";
 import { redirect } from "next/navigation";
 
 export default async function chat() {
+  let id: any = "";
   try {
     connect();
-
     const chat = new Chat({});
     await chat.save();
     console.log("Chat saved", chat);
+    id = chat._id;
   } catch (error: any) {
     console.error("Error connecting to MongoDB", error);
+  }
+  if (true) {
+    redirect(`/chat/${id}`);
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
